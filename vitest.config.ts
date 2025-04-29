42dc8d47d249325d2f457e057ace3be2eb71a2f2
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-04-29 13:49:05 +0200

    fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,14 +14,8 @@ export default defineConfig({
     include: ["./src/**/*.test.{ts,tsx}"],
     coverage: {
       provider: "v8",
-      reporter: ["text", "html", "json", "lcov"],
-      exclude: ["node_modules/", "test/setup.ts"],
-      thresholds: {
-        statements: 80,
-        branches: 70,
-        functions: 80,
-        lines: 80,
-      },
+      reporter: ["text", "html", "json"],
+      exclude: ["**/node_modules/**", "**/tests/**", "**/dist/**"],
     },
   },
   resolve: {
