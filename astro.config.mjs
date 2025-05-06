bf66cbf66bcf116210b70b2eabc3594a0a388c03
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-05-06 13:25:31 +0200

    install and use cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: cloudflare(),
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
